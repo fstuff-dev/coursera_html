b57502b8ad20215d8ad7701319a14a61717f58e9
Tommaso D'ippolito <54305048+fstuff-dev@users.noreply.github.com>
2020-08-12 14:42:45 +0200

    Update script.js
    modified

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -32,8 +32,8 @@ WARNING!!! WARNING!!!
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var name in names) {
-  var firstLetter = names[name].charAt(0);
-  if( firstLetter === "J" || firstLetter === "j")
+  var firstLetter = names[name].toLowerCase();
+  if( firstLetter === "j")
     byeSpeaker.speak(names[name]);
   else
     helloSpeaker.speak(names[name]);
