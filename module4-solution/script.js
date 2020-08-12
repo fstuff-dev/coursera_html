93031559b9a71f6f054e3a719f6b66579f2b213d
Tommaso D'ippolito <54305048+fstuff-dev@users.noreply.github.com>
2020-08-12 14:43:16 +0200

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -32,7 +32,7 @@ WARNING!!! WARNING!!!
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var name in names) {
-  var firstLetter = names[name].toLowerCase();
+  var firstLetter = names[name].charAt(0).toLowerCase();
   if( firstLetter === "j")
     byeSpeaker.speak(names[name]);
   else
